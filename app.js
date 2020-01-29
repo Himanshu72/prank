c15c8626a7bbf3eccef3f2a590e9307b3454ea7d
Himanshu72 <36104578+Himanshu72@users.noreply.github.com>
2020-01-29 16:34:50 +0530

    prank

--- app.js
+++ app.js
@@ -1,53 +1,49 @@
-const express = require('express')
-const app = express()
-const port =process.env.PORT;
-let x=1;
-app.use(express.static('audio'));
-app.get('/', (req, res) => {
-	
-	x++
-	
-	
+const express = require("express");
+const app = express();
 
-	
-	if( (Math.floor(Math.random()*10))%2==0 )
-res.send(` <meta name="viewport" content="width=device-width, initial-scale=1.0">Best OF Luck For Exam GLS...... :}<br><br> <audio id="aud" autoplay>
-  <source src="audio1.mp3" type="audio/mpeg">
-  Your browser does not support the audio element.
-</audio>
-<br>
-<input placeholder="Entername your Name:" style="width:100%; height:30px; text-align:center">
-<br><br>
-<button style="background:green; border:0; width:100%; height:30px; paddin:0 20px; " onclick="fun()"><i style="color:white">Submit</i></button>
-<script>
-function fun(){
-	document.querySelector("#aud").play();
-}
-</script>
+const gk = [
+  ["VAIO is product of:", "sony"],
+  ["Study of Inscriptions:", "Epigraphy"],
+  ["Chicken embroidery:", "Lucknow"],
+  ["Exim policy by:", "minister of commerce"],
+  ["over allowed in 1975 criket world cup :", "60"],
+  ["longest circle drawn on earth surface is :", "Equator"],
+  ["Hillstaion kadikanal in :", "Tamil Nadu"],
+  ["Miss universe in australia 2019 :", "Priya Serraro"],
+  ["Head quaters IAEA:", "Vienna,Australia"],
+  ["Country knows as percia:", "IRAN"],
+  ["Oldest Veda:", "Rigveda"],
+  ["Founding member of muslim  league:", "Nawab Salimullah Khan"],
+  ["Length of longest Railway platfrom in india:", "4483ft"]
+];
 
-`);
-else
-res.send(` <meta name="viewport" content="width=device-width, initial-scale=1.0">Best OF Luck For Exam GLS...... :}<br><br> <audio id="aud" autoplay>
-  <source src="audio.mp3" type="audio/mpeg">
-  Your browser does not support the audio element.
-</audio>
-<br>
-<input placeholder="Entername your Name:" style="width:100%; height:30px; text-align:center">
-<br><br>
-<button style="background:green; border:0; width:100%; height:30px; paddin:0 20px; " onclick="fun()"><i style="color:white">Submit</i></button>
-<script>
-function fun(){
-	document.querySelector("#aud").play();
-}
-</script>
+let gk2 = [
+  ["Grapevine is term use in relation to :", "Communication"],
+  ["SIAMESE Fight Fish  ia national Aquita animal in:", "Thailand"],
+  ["Birla indust and tech musean in:", "W.Bengal"],
+  ["First women IMO Award for Exception Bravery :", "Radhica menon"],
+  ["Energy gained when bullet fire upwars is", "Potential Energy"],
+  ["Booker price Award:", "Fiction"],
+  ["Bank merge with capital first", "IDFC"],
+  ["Five century in single cricket world cup :", "Rohit Sharma"],
+  ["Book Paradoxical PM", "Shashi Tharul"],
+  ["MDMK", "Marumalarchi Dravida Munnetra Kazhagam"],
+  ["Bank nationalize during 2nd round of nationalization in 1950", 6]
+];
 
-`);	
-	
-	
-})
-app.get("/count",(req,res)=>{
-	res.send("Counter:"+x);
-	
-})
+app.set("view engine", "ejs");
+const port = process.env.PORT || 3000;
+let x = 1;
+app.use(express.static("audio"));
+app.get("/", (req, res) => {
+  res.render("ans", { data: gk, no: 0 });
+});
+app.get("/gk", (req, res) => {
+  res.render("ans", { data: gk2, no: 13 });
+});
 
-app.listen(port, () => console.log(`Example apps listening on port ${port}!`))
+app.get("/count", (req, res) => {
+  res.send("Counter:" + x);
+});
+
+app.listen(port, () => console.log(`Example apps listening on port ${port}!`));
